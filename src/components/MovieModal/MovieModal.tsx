9eb669acafa15770a031d05b81cae5190128eef6
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-05-12 11:47:04 -0400

    fix movie modal

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
 import type { Movie } from '../../types/movie';
 import css from './MovieModal.module.css';
 
@@ -10,12 +13,43 @@ export default function MovieModal({
   movie,
   onClose,
 }: MovieModalProps) {
-  return (
-    <div className={css.backdrop} onClick={onClose}>
+  useEffect(() => {
+    function handleEscape(event: KeyboardEvent) {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    }
+
+    document.body.style.overflow = 'hidden';
+
+    window.addEventListener('keydown', handleEscape);
+
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+      document.body.style.overflow = 'auto';
+    };
+  }, [onClose]);
+
+  function handleBackdropClick() {
+    onClose();
+  }
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      onClick={handleBackdropClick}
+    >
       <div
         className={css.modal}
         onClick={(e) => e.stopPropagation()}
       >
+        <button
+          className={css.closeBtn}
+          onClick={onClose}
+        >
+          X
+        </button>
+
         <img
           className={css.image}
           src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
@@ -27,9 +61,12 @@ export default function MovieModal({
 
           <p>User score: {movie.vote_average}</p>
 
+          <p>Release date: {movie.release_date}</p>
+
           <p>{movie.overview}</p>
         </div>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 }
